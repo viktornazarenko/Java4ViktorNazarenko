d107c4064c21f76ea67ae0c4802bc1f3cb24abdd
Viktor_Nazarenko <viknaz1998@gmail.com>
2023-03-14 00:36:23 +0200

    Added two lecture objects in the feature branch

--- src/services/LectureService.java
+++ src/services/LectureService.java
@@ -12,6 +12,8 @@ public class LectureService extends Lecture {
         entities.Lecture lecture1 = new Lecture(1, "Math");
         entities.Lecture lecture2 = new Lecture(2, "English");
         entities.Lecture lecture3 = new Lecture(3, "Music");
+        entities.Lecture lecture4 = new Lecture(4, "Science");
+        entities.Lecture lecture5 = new Lecture(5, "Geography");
 
         System.out.println(count);
     }
