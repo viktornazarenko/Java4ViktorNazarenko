57ab9bed474f6b84ab9bf81ebbede8fc741c0f56
Viktor_Nazarenko <viknaz1998@gmail.com>
2023-03-14 00:37:36 +0200

    Added an object in main branch

--- src/services/LectureService.java
+++ src/services/LectureService.java
@@ -12,7 +12,7 @@ public class LectureService extends Lecture {
         entities.Lecture lecture1 = new Lecture(1, "Math");
         entities.Lecture lecture2 = new Lecture(2, "English");
         entities.Lecture lecture3 = new Lecture(3, "Music");
-
+        entities.Lecture lecture4 = new Lecture(4, "Chinese");
         System.out.println(count);
     }
 
